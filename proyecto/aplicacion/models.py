ceca807bc638c211a4a5bfb18017f27eba2c8c65
David Puerto <puertobotiadavidalejandromail.com>
2025-07-17 09:27:15 -0500

    cambio de nombre de algunas cosas

--- proyecto/aplicacion/models.py
+++ proyecto/aplicacion/models.py
@@ -7,6 +7,7 @@ import datetime
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     cedula = models.CharField(max_length=20)
+    cargo = models.CharField(max_length=50, default= "operador", choices=[('mesero', 'Mesero'), ('administrador', 'Administrador'), ('cocinero', 'Cocinero'), ("proveedor", "Proveedor")])
     correo_electronico = models.EmailField()
     numero_celular = models.CharField(max_length=20)
     estado = models.CharField(max_length=20, choices=[('activo', 'Activo'), ('inactivo', 'Inactivo')])
