e3c1a02b1ac6d4134f20a3a4e60b129cf3dabb04
Santiago Moreno <davidsanti5631@gmail.com>
2025-07-31 06:28:25 -0500

    soy la vrga

--- proyecto/aplicacion/urls.py
+++ proyecto/aplicacion/urls.py
@@ -9,7 +9,7 @@ app_name = "apl"
 urlpatterns = [
 
     path("index/", vista1, name="index"),
-    path("p/", vista2,name="p"),
+    #_________________________ Modulos de Usuario __________________________
     path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
     path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='editar_usuario'),
     path('usuarios/eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='eliminar_usuario'),
@@ -19,4 +19,6 @@ urlpatterns = [
     path('categorias/crear/', CategoriaCreateView.as_view(), name='crear_categoria'),
     path('categorias/editar/<int:pk>/', CategoriaUpdateView.as_view(), name='editar_categoria'),
     path('categorias/eliminar/<int:pk>/', CategoriaDeleteView.as_view(), name='eliminar_categoria'),
+    #
+    
 ]
