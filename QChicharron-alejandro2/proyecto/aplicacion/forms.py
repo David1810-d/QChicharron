39492e4e8935ae92b72486abdba28c84de05cb66
Santiago Moreno <davidsanti5631@gmail.com>
2025-09-04 09:31:04 -0500

    Primer commit - corrección

---
+++ QChicharron-alejandro2/proyecto/aplicacion/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from aplicacion.models import *
+from django.forms import inlineformset_factory
+from django_select2.forms import *
+
+#class MensajeForm(forms.ModelForm):
+#   class Meta:
+#       model = Mensaje
+#       fields = ['nombre', 'contenido']
+#ewfergergf
+
+
+class PlatoForm(forms.ModelForm):
+    class Meta:
+        model = Plato
+        fields = ['nombre', 'descripcion', 'precio']
+
+
+class PlatoProductoForm(forms.ModelForm):
+    class Meta:
+        model = PlatoProducto
+        fields = ['producto', 'cantidad', 'unidad']
+        widgets = {
+            'producto': Select2Widget,
+        }
+
+PlatoProductoFormSet = inlineformset_factory(
+    Plato,
+    PlatoProducto,
+    form=PlatoProductoForm,
+    extra=1,
+    can_delete=True
+)
+#sdfgdfhfg
